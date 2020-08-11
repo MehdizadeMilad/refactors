8680c972af24add625df3462b5f6ae2fd38cb0ed
mehdizademilad <milad.mehdizade1394@gmail.com>
2020-08-12 01:24:50 +0430

    Refactor 2.js

--- 2.js
+++ 2.js
@@ -1,78 +1,61 @@
-const User = require('../../../modules/user/models').User;
-const { promisify } = require('util');
+const { User } = require('../../user/models');
+const { Cache } = require('../../../helpers');
 
 module.exports = {
 
-    //Complexity 29
+    //Complexity 18
     async lastMatchDetails(req, res) {
 
-        //...
+        // ...
 
         // Get Opponent User info from cache
         let userId = req.user.id; // Validation done.
+        let thisPlayerNo = null;
+        let opponentPlayerNo = null;
+
+        if (userId === match.Player1Id) {
+            thisPlayerNo = 'player1';
+            opponentPlayerNo = 'player2';
+        }
+        else {
+            thisPlayerNo = 'player2';
+            opponentPlayerNo = 'player1';
+        }
+
         let opponent = null;
-
-        const getAsync = promisify(client.get).bind(client);
-        await getAsync('game-' + match.token).then(result => {
-            if (result !== null) {
-                let json_result = JSON.parse(result);
-                if (json_result !== null) {
-                    opponent = json_result.player1 === userId ? json_result.player2_info : json_result.player1_info;
-                }
-            }
-        });
-
-        if (!opponent) {
+        let gameData = await Cache.get('game-' + match.token);
+        if (gameData) {
+            opponent = gameData[`${opponentPlayerNo}_info`]
+        }
+        else {
             opponent = await User.findByPk(opponentUserId, {
                 //...
             });
         }
 
         // does this user get's The Winner extra score?
-        let win_score = match.Player1Id === userId ? (match.player1_score > match.player2_score ? 40 : 0) : (match.player2_score > match.player1_score ? 40 : 0);
+        winScore = (match[`${thisPlayerNo}_score`] > match[`${opponentPlayerNo}_score`] ? 40 : 0);
 
         // correctAnswers = Count of correct answers in all user's answer
         // timeAnswer = Sum of all seconds user used to answer each question
         let correctAnswers = 0;
         let timeAnswer = 0;
-        if (match.Player1Id === userId) {
-            correctAnswers = match.MatchQuizzes.filter(quiz => {
-                return quiz.player1_score > 0;
-            }).length;
-
-            timeAnswer = match.MatchQuizzes.reduce(function (cnt, o) { return cnt + o.player1_time; }, 0);
-        }
-        else {
-            correctAnswers = match.MatchQuizzes.filter(quiz => {
-                return quiz.player2_score > 0;
-            }).length;
-            timeAnswer = match.MatchQuizzes.reduce(function (cnt, o) { return cnt + o.player2_time; }, 0);
-        }
+        correctAnswers = match.MatchQuizzes.filter(m => m[`${thisPlayerNo}_score`]).length;
+        timeAnswer = match.MatchQuizzes.reduce((total, currentItem) => total + currentItem[`${thisPlayerNo}_time`], 0);
 
         // ...
     },
 
-    //Complexity 5
-    // a Question has 4 options
-    // 3 are Wrong
-    // 1 is Correct
-    // we need to select 2 random wrong options
+    //Complexity 3
     async use_50_50(user, gameToken, questionId) {
 
-        // ... 
+        // ...
 
-        questions.forEach(item => {
-            if (item.question.id === parseInt(questionId)) {
-                while (incorrect_options.length < 2) {
-                    let rnd = parseInt(Math.random() * item.options.length);
-                    if (!item.options[rnd].is_correct) {
-                        incorrect_options.push(item.options[rnd].id);
-                        item.options.splice(rnd, 1);
-                    }
-                }
-            }
-        });
+        let currentQuestionOptions = (questions.filter(q => q.question.id === questionId)[0]).options.filter(o => !o.is_correct)
+        let wrongOptions = [];
+        wrongOptions.push(currentQuestionOptions.splice(Math.floor(Math.random() * 3), 1)[0].id);
+        wrongOptions.push(currentQuestionOptions.splice(Math.floor(Math.random() * 2), 1)[0].id);
 
-        //...
+        // ...
     },
 };
