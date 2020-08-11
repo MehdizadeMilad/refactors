e3be9624e51094c4642493237165a45ce8681543
mehdizademilad <milad.mehdizade1394@gmail.com>
2020-08-11 23:33:24 +0430

    Refactor Imports ; Separate Helper methods;
    Refactor User's answer in cache data structure;
    
    Use Bulk Create instead of loop;
    
    Use async/await instead of promisify;
    
    Improve Exception handling (log it);

--- 1.js
+++ 1.js
@@ -1,194 +1,154 @@
-const User = require('../../../modules/user/models').User;
-const MainMatch = require('../models').MainMatch;
-const MainMatchQuiz = require('../models').MainMatchQuiz;
-const ReserveMatch = require('../models').ReserveMatch;
-const ReserveMatchDetails = require('../models').ReserveMatchDetails;
-const { promisify } = require('util');
-const FCM = require('../../../functions/fcm');
+const { User } = require('../../../modules/user/models');
+const { MainMatch, MainMatchQuiz, ReserveMatch, ReserveMatchDetails } = require('../models');
+const { Cache, UserActivity, Constants, Common, FCM, Notification } = require('../../../helpers');
+
 const models = require('../models');
 const Op = models.Sequelize.Op;
 const db = require('../models/index');
-const UsableFunctions = require('../../../functions/usable_functions');
-const ApiFunctions = require('../../../functions/api_functions');
-let hostname = 'http://api.example.com';
 
-const redis = require('../../../functions/redis');
-
+const config = require('../../../config/config.json');
 
 module.exports = {
 
-    //Complexity 9
+    //Complexity 12
     async startMatch(sockets) {
         let minuteAgo = new Date();
         minuteAgo.setTime(minuteAgo.getTime() - 60000);
-        let mainMatch = await MainMatch.findOne({
-            where: {
-                // ...
-            },
-        });
+        try {
+            let mainMatchInfo = await MainMatch.findOne({
+                where: {
+                    //...
+                },
+            });
+            if (!mainMatchInfo) return;
 
-        if (mainMatch) {
-            console.log('main match found, id: ' + mainMatch.id);
             let questions = await MainMatchQuiz.findAll({
                 where: {
                     //...
                 },
-                order: models.Sequelize.literal('random()')
             });
+            await Cache.set(Constants.MainMatch.Events.Questions + mainMatchInfo.id, questions, 2400);
 
-            let data = {
-                questions,
-                user_answers: [],
-            };
-
-            client.set('main-match-' + mainMatch.id, JSON.stringify(data), 'EX', 2400);
-
-            let mapped_questions = questions.map(question => {
-                let random_opts = question.options.map(opt => {
+            questions = questions.map(question => {
+                let optionsWithoutAnswer = question.options.map(opt => {
                     return {
                         id: opt.id,
                         option: opt.option
                     }
                 });
-
-                random_opts = UsableFunctions.shuffle_array(random_opts);
-
                 return {
                     id: question.id,
                     text: question.question,
-                    image: question.image_name ? hostname + '/images/questions/' + question.image_name : '',
-                    audio: question.audio_file_name ? hostname + '/audio/questions/' + question.audio_file_name : '',
-                    options: random_opts
+                    image: question.image_name ? (config.hostname + '/images/questions/' + question.image_name) : '',
+                    audio: question.audio_file_name ? (config.hostname + '/audio/questions/' + question.audio_file_name) : '',
+                    options: Common.shuffle_array(optionsWithoutAnswer)
                 }
             });
 
-            let room_id = 'room_main_match' + mainMatch.id;
-            for (let i = 0; i < mapped_questions.length; i++) {
-                client.set('current_question_index-' + mainMatch.id, JSON.stringify({ index: i }), 'EX', 2400);
-                await module.exports.delay_question(sockets, mainMatch.id, room_id, mapped_questions, i);
+            let room_id = Constants.MainMatch.Events.Room + mainMatchInfo.id;
+            for (let i = 0; i < questions.length; i++) {
+                await Cache.set(Constants.MainMatch.Events.CurrentQuestionIndex + mainMatchInfo.id, { index: i }, 2400);
+                await module.exports.delay_question(sockets, mainMatchInfo.id, room_id, questions, i);
             }
 
-            module.exports.finishMatch(mainMatch.id);
+            module.exports.finishMatch(mainMatchInfo.id);
+        }
+        catch (error) {
+            console.log(error);
         }
     },
 
-    //Complexity 30
-    async set_answer(user, data) {
+    //Complexity 21
+    async answer(user, data) {
         if (!(data.main_match_id && data.question_id)) {
+            console.log('param error');
             return false;
         }
-        else {
-            const getAsync = promisify(client.get).bind(client);
 
-            let currentQuestion = await getAsync('current_question_index-' + data.main_match_id);
-            let currentQuestionIndex = JSON.parse(currentQuestion).index || 1;
+        let currentQuestionIndex = await Cache.get(Constants.MainMatch.Events.CurrentQuestionIndex + data.main_match_id);
+        currentQuestionIndex = currentQuestionIndex.index;
 
-            let isCorrect = false;
-            let score = 0;
-            getAsync('main-match-' + data.main_match_id).then(result => {
-                if (result !== null) {
-                    let jsonResult = JSON.parse(result);
-                    if (jsonResult !== null) {
-                        let question = jsonResult.questions.filter(question => {
-                            if (question.id === data.question_id) {
-                                return question
-                            }
-                        })[0];
+        let isCorrect = 0;
+        let score = 0;
+        try {
 
-                        if (data.option_id !== 0) {
-                            let option = question.options.filter(opt => {
-                                if (opt.id === data.option_id) {
-                                    return opt;
-                                }
-                            })[0];
+            let questions = await Cache.get(Constants.MainMatch.Events.Questions + data.main_match_id);
 
-                            if (option) {
-                                isCorrect = option.is_correct;
-                                score = ApiFunctions.calculateScore(data.time, isCorrect, false);
-                            }
-                        }
+            let currentQuestion = questions.filter(q => (q.id === data.question_id))[0];
 
-                        if (data.option_id !== 0 && !isCorrect && currentQuestionIndex >= 10) {
-                            score -= 5;
-                        }
+            if (data.option_id) {
+                let userSelectedOption = currentQuestion.options
+                    .filter(opt => (opt.id === data.option_id))[0];
 
-                        UsableFunctions.emitToClient(user.id, 'main_match_question_result', {
-                            //...
-                        });
-
-                        console.log('main_match_question_result');
-                        console.log({
-                            question_id: data.question_id,
-                            score: score,
-                            option: data.option_id,
-                            is_correct: !!isCorrect,
-                        });
-
-                        let isAnswered = jsonResult.user_answers.filter(answer => {
-                            if (answer.user_id === user.id && answer.question_id === data.question_id) {
-                                return answer;
-                            }
-                        })[0];
-
-                        if (!isAnswered) {
-                            jsonResult.user_answers.push({
-                                user_id: user.id,
-                                question_id: data.question_id,
-                                option_id: data.option_id,
-                                is_correct: isCorrect,
-                                time: data.time,
-                                score: score
-                            });
-                            client.set('main-match-' + data.main_match_id, JSON.stringify(jsonResult), 'EX', 2400); // expired in 20 minutes later
-                        }
-                        else {
-                        }
-
-                    }
-                    console.log(jsonResult);
+                if (userSelectedOption) {
+                    isCorrect = userSelectedOption.is_correct;
+                    score = UserActivity.calculateScore(data.time, isCorrect, false);
+                    if (!isCorrect && currentQuestionIndex >= 10) score -= 5;
                 }
-                else {
-                    return false;
-                }
-            })
-                .catch(e => {
-                    return false;
+            }
+
+            Notification.emitToClient(user.id, Constants.MainMatch.Events.QuestionResult, {
+                // ...
+            });
+
+            // Read from User specific record
+            let currentUserAnswers = await Cache.get(Constants.MainMatch.Events.UserAnswers + user.id);
+
+            let isAnswered = currentUserAnswers
+                .filter(a => (a.user_id === user.id && a.question_id === data.question_id))[0];
+
+            if (!isAnswered) {
+                currentUserAnswers.push({
+                    user_id: user.id,
+                    question_id: data.question_id,
+                    option_id: data.option_id,
+                    is_correct: isCorrect,
+                    time: data.time,
+                    score: score
                 });
+
+                // Write to User specific record
+                await Cache.set(Constants.MainMatch.Events.UserAnswers + user.id, currentUserAnswers, 2400);
+            }
+        }
+        catch (err) {
+            console.log('main match SetAnswer error:', err);
         }
     },
 
-    //Complexity 9
+    //Complexity 6
     async finishMatch(main_match_id) {
 
-        let reservedMatch = await ReserveMatch.findAll({
-            where: {
-                //...
-            }
-        });
+        let reservedUsers = await ReserveMatch.findAll({ where: { MainMatchId: main_match_id } });
 
-        const getAsync = promisify(client.get).bind(client);
-        getAsync('main-match-' + main_match_id).then(result => {
-            if (result !== null) {
-                let jsonResult = JSON.parse(result);
-                if (jsonResult !== null) {
-                    Promise.all(reservedMatch.map(async reserve => {
-                        let totalScore = 0;
-                        jsonResult.user_answers.filter(ans => {
-                            if (ans.user_id === reserve.UserId) {
-                                return ans;
-                            }
-                        }).forEach(answer => {
-                            totalScore += answer.score;
-                            ReserveMatchDetails.create({
-                                ReserveMatchId: reserve.id,
-                                MainMatchQuizId: answer.question_id,
-                                time: answer.time,
-                                score: answer.score
-                            })
+        Promise.all(
+            reservedUsers.map(async ru => {
+                let totalScore = 0;
+                let totalAnswers = [];
+
+                let userAnswers = await Cache.get(Constants.MainMatch.Events.UserAnswers + ru.UserId)
+                if (userAnswers) {
+                    userAnswers.forEach(answer => {
+                        totalScore += answer.score;
+                        totalAnswers.push({
+                            ReserveMatchId: ru.id,
+                            MainMatchQuizId: answer.question_id,
+                            time: answer.time,
+                            score: answer.score
                         });
-                    }));
+                    });
+                    await ReserveMatchDetails.bulkCreate(totalAnswers)
+                        .catch(err => {
+                            console.log('bulk MainMatch failed', err);
+                            console.log('TOTAL Answers:', JSON.stringify(totalAnswers));
+                        });
+
                 }
-            }
-        });
+                else {
+                    console.log('user answers not found!', ru.UserId)
+                }
+            })).catch(err => {
+                console.log(err);
+            });
     },
-}
+};
